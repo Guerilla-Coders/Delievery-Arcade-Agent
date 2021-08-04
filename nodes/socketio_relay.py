ba2e2271067235e19ab0d487f20214d9e1595f0f
earthicko <earthicko@gmail.com>
2021-08-04 23:45:52 +0900

    Agent 컨트롤러 관련 기능 별도 모듈로 분리

---
+++ nodes/socketio_relay.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+import sys
+import os
+import select
+import tty
+import termios
+from da_pkg.network_config import NetworkConfig
+from da_pkg.consts import Limits
+from da_pkg.physics_processing import make_simple_profile
+from da_pkg.agent import DeliveryArcadeAgent
+
+
+def get_key():
+    """키보드 좌판 입력을 받아오는 함수"""
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key_input = sys.stdin.read(1)
+    else:
+        key_input = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key_input
+
+
+if __name__ == "__main__":
+    settings = termios.tcgetattr(sys.stdin)
+
+    node_dir = os.path.dirname(__file__)
+    config_file_path = "network_config.json"
+    abs_config_file_path = os.path.join(node_dir, config_file_path)
+
+    print(f"Loading network config file from {abs_config_file_path}")
+    network_config = NetworkConfig(abs_config_file_path)
+    print(f"Control server URI: http://{network_config.ip}:{network_config.ports.control}")
+
+    # DeliveryArcadeAgent instance generated
+    Robot = DeliveryArcadeAgent()
+    rospy.loginfo("Created DeliveryArcadeAgent()")
+
+    try:
+        while True:
+            # get keyboard input
+            key = get_key()
+            if key == 'w':
+                Robot.add_front_velocity()
+            elif key == 'x':
+                Robot.add_back_velocity()
+            elif key == 'a':
+                Robot.add_CCW_velocity()
+            elif key == 'd':
+                Robot.add_CW_velocity()
+            elif key == ' ' or key == 's':
+                Robot.stop()
+            elif key == '\x03':
+                break
+
+            # set final control linear and angular velocities
+            Robot.control_linear_vel = make_simple_profile(Robot.control_linear_vel, Robot.target_linear_vel,
+                                                           (Limits.LIN_VEL_STEP_SIZE / 2.0))
+            Robot.control_angular_vel = make_simple_profile(Robot.control_angular_vel, Robot.target_angular_vel,
+                                                            (Limits.ANG_VEL_STEP_SIZE / 2.0))
+
+            # formulate twist msg
+            Robot.make_twist_data()
+
+            # publish twist via 'cmd_vel' topic
+            Robot.do_publishing()
+
+    except BaseException as error:
+        print(f"Exception {error}")
+
+    finally:
+        Robot.terminator()
+        print("Warning : Teleoperation Shutdown!\n")
+        Robot.do_publishing()
+
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
--- src/da_pkg/agent.py
+++ src/da_pkg/agent.py
@@ -1,27 +1,7 @@
-#!/usr/bin/env python3
-
 import rospy
 from geometry_msgs.msg import Twist
-import sys
-import os
-import select
-import tty
-import termios
 from da_pkg.consts import Limits
-from da_pkg.physics_processing import make_simple_profile, constrain
-from da_pkg.network_config import NetworkConfig
-
-
-def get_key():
-    """키보드 좌판 입력을 받아오는 함수"""
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-    if rlist:
-        key_input = sys.stdin.read(1)
-    else:
-        key_input = ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key_input
+from da_pkg.physics_processing import constrain
 
 
 class DeliveryArcadeAgent:
@@ -44,7 +24,7 @@ class DeliveryArcadeAgent:
 
     """
     BEWARE OF VELOCITY CONCEPTS!
-    
+
     Think about physics, use your right-hand-rule to check the positive directions of unit vectors.
     In addition, please remind that CounterClockwise(CCW) is positive and Clockwise(CW) is negative when it comes to angular velocity.
 
@@ -112,64 +92,3 @@ class DeliveryArcadeAgent:
         self.twist.angular.x = 0.0
         self.twist.angular.y = 0.0
         self.twist.angular.z = 0.0
-
-
-#######################
-# For clearer mangement#
-#######################
-
-"""ACTUAL MANAGING AREA"""
-
-if __name__ == "__main__":
-    settings = termios.tcgetattr(sys.stdin)
-
-    node_dir = os.path.dirname(__file__)
-    config_file_path = "network_config.json"
-    abs_config_file_path = os.path.join(node_dir, config_file_path)
-
-    print(f"Loading network config file from {abs_config_file_path}")
-    network_config = NetworkConfig(abs_config_file_path)
-    print(f"Control server URI: http://{network_config.ip}:{network_config.ports.control}")
-
-    # DeliveryArcadeAgent instance generated
-    Robot = DeliveryArcadeAgent()
-    rospy.loginfo("Created DeliveryArcadeAgent()")
-
-    try:
-        while True:
-            # get keyboard input
-            key = get_key()
-            if key == 'w':
-                Robot.add_front_velocity()
-            elif key == 'x':
-                Robot.add_back_velocity()
-            elif key == 'a':
-                Robot.add_CCW_velocity()
-            elif key == 'd':
-                Robot.add_CW_velocity()
-            elif key == ' ' or key == 's':
-                Robot.stop()
-            elif key == '\x03':
-                break
-
-            # set final control linear and angular velocities
-            Robot.control_linear_vel = make_simple_profile(Robot.control_linear_vel, Robot.target_linear_vel,
-                                                           (Limits.LIN_VEL_STEP_SIZE / 2.0))
-            Robot.control_angular_vel = make_simple_profile(Robot.control_angular_vel, Robot.target_angular_vel,
-                                                            (Limits.ANG_VEL_STEP_SIZE / 2.0))
-
-            # formulate twist msg
-            Robot.make_twist_data()
-
-            # publish twist via 'cmd_vel' topic
-            Robot.do_publishing()
-
-    except BaseException as error:
-        print(f"Exception {error}")
-
-    finally:
-        Robot.terminator()
-        print("Warning : Teleoperation Shutdown!\n")
-        Robot.do_publishing()
-
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
